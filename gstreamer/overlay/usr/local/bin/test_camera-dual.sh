3cf62121fa7645a020c2a767cf12f0f91644b36d
leeboby <leeboby@aliyun.com>
2021-02-05 15:54:37 +0800

    Update test_camera-dual.sh

--- gstreamer/overlay/usr/local/bin/test_camera-dual.sh
+++ gstreamer/overlay/usr/local/bin/test_camera-dual.sh
@@ -8,13 +8,11 @@ echo "Start MIPI CSI Camera Preview!"
 
 #echo 1 > /sys/module/video_rkisp1/parameters/rkisp1_debug
 
-gst-launch-1.0 rkisp device=/dev/video1 io-mode=1 analyzer=1 enable-3a=1 path-iqf=/etc/cam_iq.xml \
-   ! video/x-raw,format=NV12,width=1280,height=720, framerate=30/1 ! videoconvert ! autovideosink &
-
+su orangepi -c "gst-launch-1.0 rkisp device=/dev/video0 io-mode=1 analyzer=1 enable-3a=1 path-iqf=/etc/cam_iq.xml \
+   ! video/x-raw,format=NV12,width=1280,height=720, framerate=30/1 ! videoconvert ! autovideosink" &
 
 sleep 1
 
 
-gst-launch-1.0 rkisp device=/dev/video6 io-mode=1 analyzer=1 enable-3a=1 path-iqf=/etc/cam_iq.xml \
-    ! video/x-raw,format=NV12,width=1280,height=720, framerate=30/1 ! videoconvert ! autovideosink 
-
+su orangepi -c "gst-launch-1.0 rkisp device=/dev/video5 io-mode=1 analyzer=1 enable-3a=1 path-iqf=/etc/cam_iq.xml \
+    ! video/x-raw,format=NV12,width=1280,height=720, framerate=30/1 ! videoconvert ! autovideosink"
